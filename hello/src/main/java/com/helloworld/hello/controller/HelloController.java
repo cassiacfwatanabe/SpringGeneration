29fb57246a541b40a8fab528951e1cd2896a1e56
unknown <you@example.com>
2022-01-18 15:09:55 -0300

    Primeiros Exercícios

---
+++ hello/src/main/java/com/helloworld/hello/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.helloworld.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/hello")
+
+public class HelloController {
+	
+	@GetMapping
+	public String hello() {
+		return " Persistência e Atenção aos detalhes ";
+	}
+	
+
+}
---
+++ objetivos/objetivos/src/main/java/com/objetivos/objetivos/controller/ObjetivosController.java
@@ -0,0 +1,17 @@
+package com.objetivos.objetivos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/objetivos")
+
+public class ObjetivosController {
+	
+	@GetMapping
+	public String objetivo() {
+		return "Meus objetivos são de me superar e sempre aprender";
+	}
+
+}
